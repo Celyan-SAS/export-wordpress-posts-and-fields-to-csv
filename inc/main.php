ded259e5b7cded216c6408d06cc434b2571c8574
yannbug <yann@abc.fr>
2017-10-09 17:07:14 +0200

    Init

--- inc/main.php
+++ inc/main.php
@@ -14,6 +14,32 @@ class wpExportPFCSV {
 	 */
 	public function __construct() {
 		
+		/** Plugin admin page for export button **/
+		add_action( 'admin_menu', array( $this, 'plugin_admin_add_page' ) );
+	}
+	
+	/**
+	 * Adds the plugin settings page
+	 * 
+	 */
+	public function plugin_admin_add_page() {
+
+		add_menu_page( 
+			__( 'Export WP posts and fields to CSV', 'wpexportpfcsv' ),	// Page title
+			__( 'Export to CSV', 'wpexportpfcsv' ), 					// Menu title
+			'manage_options', 											// Capability
+			'wpexportpfcsv', 											// Menu slug
+			array( $this, 'plugin_options_page'	)						// Method
+		);
+	}
+	
+	/**
+	 * Pugin admin page for export button
+	 * 
+	 */
+	public function plugin_options_page() {
+		
+		echo '<h2>Hello World</h2>';
 	}
 }
 ?>
