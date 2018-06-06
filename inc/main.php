fd21dc68291a3688fa4760c085c1a9bb318b6838
ydubois <yann@abc.fr>
2018-06-06 16:36:37 +0200

    Any post status

--- inc/main.php
+++ inc/main.php
@@ -123,7 +123,7 @@ class wpExportPFCSV {
 		}
 		$data = '';
 		
-		if( $posts = get_posts( array( 'post_type'=>$post_type, 'posts_per_page'=>-1, 'lang'=>'' ) ) ) {
+		if( $posts = get_posts( array( 'post_type'=>$post_type, 'posts_per_page'=>-1, 'post_status'=>'any', 'lang'=>'' ) ) ) {
 			
 			$count = count( $posts );
 			
